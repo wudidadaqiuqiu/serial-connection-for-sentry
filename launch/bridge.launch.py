0294f170a640603f6f5e365a8303e296c728967a
CR_Raphael <1234567@qqqq.com>
2024-03-26 11:08:15 +0800

    给串口驱动加launch

---
+++ launch/bridge.launch.py
@@ -0,0 +1,26 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory, get_package_prefix
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition
+from launch_ros.actions import Node
+from launch.substitutions import Command
+from launch.actions import ExecuteProcess
+from launch.actions import TimerAction
+
+
+def generate_launch_description():
+
+    serial_connection_node = Node(
+        package='serial_connection',
+        executable='connector',
+        output='screen'
+    )
+
+    ld = LaunchDescription()
+
+    ld.add_action(serial_connection_node)
+
+    return ld
