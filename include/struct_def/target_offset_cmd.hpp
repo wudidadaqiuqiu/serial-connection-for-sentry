4048ec86473618d9dcc435b712c93e494f81c48c
xy <wudidadaqiuqiu@qq.com>
2024-05-22 10:23:31 +0800

    add autoaim mode field in target offset

--- include/struct_def/target_offset_cmd.hpp
+++ include/struct_def/target_offset_cmd.hpp
@@ -13,16 +13,29 @@ namespace eamsg = easy_robot_commands::msg;
 template <>
 struct EasyRobotCommands::StructDataT<robot_msgs::msg::CamCommand> {
 #pragma pack(1)
+    enum auto_shoot_mode_e : uint8_t{
+        auto_aim_off = 0, 
+        auto_aim_normal, // 普通自瞄
+        auto_aim_outpost,
+        auto_aim_outpost_up,
+        auto_aim_buff_small,// 小符
+        auto_aim_buff_big,//大符
+        // auto_aim_anti_buff,//反符
+        autoaim_stop,
+        autoaim_unkonw,
+        auto_aim_mode_num,
+    };
+
     struct data_t {
         int16_t ref_offset;
         uint8_t pitch_mode;
         uint8_t autoaim_rate;
         uint8_t priority_type_arr[8];
         uint8_t priority_level_arr[8];
-        
+        auto_shoot_mode_e autoaim_mode;
     };
     data_t data;
-    static_assert((sizeof(data_t) == 20));
+    static_assert((sizeof(data_t) == 21));
 #pragma pack()
     /* assign operate*/
     StructDataT& operator=(const robot_msgs::msg::CamCommand::SharedPtr msgptr) {
@@ -34,12 +47,13 @@ struct EasyRobotCommands::StructDataT<robot_msgs::msg::CamCommand> {
 
         data.pitch_mode = msgptr->pitch_mode;
         data.autoaim_rate = msgptr->autoshoot_rate;
-        
+
         try {
             for (int i = 0; i < 8; ++i) {
                 data.priority_type_arr[i] = msgptr->priority_type_arr.at(i);
                 data.priority_level_arr[i] = msgptr->priority_level_arr.at(i);
             }
+            *(uint8_t*)&data.autoaim_mode = msgptr->autoaim_mode;
         } catch (std::exception& e) {
             std::cerr << "!!!!!!!!!!!!!!!!        arr must be 8 len     !!!!!!!!!!!!!!" << std::endl;
             std::cerr << "\n\n";
